4c94cbca317901e50c91e7dca2e1bc7778f84cc8
iparknoori <iparknoori01@gmail.com>
2024-05-02 10:46:37 +0900

    hello bosung

--- src/main/java/com/soldesk/project/testClass.java
+++ src/main/java/com/soldesk/project/testClass.java
@@ -3,7 +3,8 @@ package com.soldesk.project;
 public class testClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Hello, this is a project!");
 
 	}
 
